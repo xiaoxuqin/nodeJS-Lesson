73aa9ede19cf17ea96e1aca54a28fd2c48734158
秦晓旭 <33643037+xiaoxuqin@users.noreply.github.com>
2019-09-05 20:21:26 +0800

    Add files via upload

---
+++ 01 NodeJS简介/实验一/helloWorldServer.js
@@ -0,0 +1,9 @@
+const http = require("http");
+var server = http.createServer(function(req,res){
+    res.writeHead(200, {"Content-Type":"text/html"});
+    res.write("<h1>hello world!</h1>");
+    res.end();
+});
+server.listen(8080);
+console.log("server is listening 8080");
+
