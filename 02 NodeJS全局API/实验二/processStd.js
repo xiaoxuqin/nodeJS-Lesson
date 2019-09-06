21638eaf133f84af43170d5d85c5b82d3998771e
秦晓旭 <33643037+xiaoxuqin@users.noreply.github.com>
2019-09-06 09:11:03 +0800

    Add files via upload

---
+++ 02 NodeJS全局API/实验二/processStd.js
@@ -0,0 +1,12 @@
+// 实验2 程序⑦
+// var i = 0;
+// process.stdin.on("data", function(data){
+//     i++;
+//     if(i == 4){
+//         process.exit();
+//     }else{
+//         // console.log(data.toString());
+//         console.log(obj[i]);
+//     }
+// })
+// 敲击会车的时候会输出二进制数据
