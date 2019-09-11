fe25623ae5a6e6fff32a56f7ed273f877f7026c7
秦晓旭 <33643037+xiaoxuqin@users.noreply.github.com>
2019-09-11 08:18:11 +0800

    Update mainModule.js

--- 02 NodeJS全局API（下）/实验三/circleFirst/mainModule.js
+++ 02 NodeJS全局API（下）/实验三/circleFirst/mainModule.js
@@ -11,3 +11,10 @@ var circleObj = circleModule.circleFun(r);
 console.log("圆的半径：" + r);
 console.log("圆的周长：" + circleObj.circumference());
 console.log("圆的面积：" + circleObj.area());
+
+/**
+ * 原生模块：随node环境安装就存在
+ * 自定义模块：自己编写的程序
+ * 第三方模块：在命令行中进行安装的模块，从npm服务器上下载到本地的
+ *            直接require("data-now");
+ */
